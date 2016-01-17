1b6132aa6c84c506a189ebef7f80ed584b553a5c
zhenjing <zhenjinglee@gmail.com>
2016-01-17 22:52:27 +0800

    update examples

--- src/controllers/main.go
+++ src/controllers/main.go
@@ -4,6 +4,17 @@ import (
 	"github.com/singpenguin/mango"
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func IndexGET(ctx *mango.HTTPRequest) {
-	ctx.Write("Hello World")
+	//ctx.Write("Hello World")
+	//return
+	p := Person{"singpenguin", 27}
+	ctx.SetHeader("x-test", "test")
+	//ctx.SetCookie("uname", "singpenguin", 30)
+	ctx.SetSecureCookie("session", "is_login", 600)
+	ctx.Render("index.html", p)
 }
