89f66b4fbf0f8d5d1b04cf5790277a5c366d075d
zhenjing <zhenjinglee@gmail.com>
2017-01-09 15:02:24 +0800

    update mango examples

--- src/config/url.go
+++ src/config/url.go
@@ -9,4 +9,8 @@ import (
 var Urls = map[string]map[string]mango.Handler{
 	"/":       map[string]mango.Handler{"GET": IndexGET},
 	"/(\\d+)": map[string]mango.Handler{"GET": IndexGET},
+
+	"/insert": map[string]mango.Handler{"POST": PostHandler},
+	"/update": map[string]mango.Handler{"PUT": PutHandler},
+	"/delete": map[string]mango.Handler{"DELETE": DeleteHandler},
 }
--- src/controllers/main.go
+++ src/controllers/main.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/singpenguin/mango"
 )
 
@@ -10,11 +11,39 @@ type Person struct {
 }
 
 func IndexGET(ctx *mango.HTTPRequest) {
-	//ctx.Write("Hello World")
+	//ctx.Write([]byte("Hello World"))
 	//return
 	p := Person{"singpenguin", 27}
 	ctx.SetHeader("x-test", "test")
 	//ctx.SetCookie("uname", "singpenguin", 30)
 	ctx.SetSecureCookie("session", "is_login", 600)
 	ctx.Render("index.html", p)
+}
+
+func PostHandler(ctx *mango.HTTPRequest) {
+	id := ctx.Params.Get("id")
+	fmt.Println(id)
+	if id == "" {
+		ctx.Write([]byte("id cannot be empty"))
+		return
+	}
+	ctx.Write([]byte("insert ok: " + id))
+}
+
+func PutHandler(ctx *mango.HTTPRequest) {
+	id := ctx.Params.Get("id")
+	if id == "" {
+		ctx.Write([]byte("id cannot be empty"))
+		return
+	}
+	ctx.Write([]byte("update ok: " + id))
+}
+
+func DeleteHandler(ctx *mango.HTTPRequest) {
+	id := ctx.Params.Get("id")
+	if id == "" {
+		ctx.Write([]byte("id cannot be empty"))
+		return
+	}
+	ctx.Write([]byte("delete ok: " + id))
 }
